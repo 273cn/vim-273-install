b28869e8794f5d408078c7da69d69cff912cf221
daiyc <daiyc@273.cn>
2015-05-13 04:37:02 +0000

    vim

---
+++ install.sh
@@ -0,0 +1,16 @@
+base_url='https://raw.githubusercontent.com/274cn/vim-273-install/master/'
+
+if [ ! -f ~/.vimrc.bak ]; then
+    mv ~/.vimrc ~/.vimrc.bak
+fi
+
+rm -f ~/.vimrc.bundles ~/.vimrc.bundles.local
+mkdir ~/.vim
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+wget "${base_url}vim/vimrc" -O ~/.vimrc
+wget "${base_url}vim/vimrc.bundles" -O ~/.vimrc.bundles
+wget "${base_url}vim/vimrc.bundles.local" -O ~/.vimrc.bundles.local
+
+vim +BundleInstall
